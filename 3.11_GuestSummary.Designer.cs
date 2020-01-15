23bc76c3820e160fee47fe0d123eeb245dd3825e
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-01-15 22:35:45 +0800

    Completed 3.11

--- 3.11_GuestSummary.Designer.cs
+++ 3.11_GuestSummary.Designer.cs
@@ -28,9 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.backBtn = new System.Windows.Forms.Button();
@@ -107,20 +108,26 @@
             this.cbTypeGuest.Name = "cbTypeGuest";
             this.cbTypeGuest.Size = new System.Drawing.Size(239, 33);
             this.cbTypeGuest.TabIndex = 8;
+            this.cbTypeGuest.SelectedIndexChanged += new System.EventHandler(this.cbTypeGuest_SelectedIndexChanged);
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(153, 248);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series3.Legend = "Legend1";
+            series3.Name = "delegates";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series4.Legend = "Legend1";
+            series4.Name = "competitors";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
             this.chart1.Size = new System.Drawing.Size(680, 336);
             this.chart1.TabIndex = 9;
             this.chart1.Text = "chart1";
---
+++ 3.11_GuestSummary.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Session3
+{
+    public partial class GuestSummary : Form
+    {
+        public GuestSummary()
+        {
+            InitializeComponent();
+        }
+
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            (new AdminMain()).ShowDialog();
+            this.Close();
+        }
+
+        private void GuestSummary_Load(object sender, EventArgs e)
+        {
+            cbTypeGuest.SelectedItem = "No Filter";
+            LoadGraph();
+        }
+        private void LoadGraph()
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            if (cbTypeGuest.SelectedItem.ToString() == "No Filter")
+            {
+                using (var context = new Session3Entities())
+                {
+                    
+                    var getCountries = (from x in context.Users
+                                        select x.countryName).Distinct();
+                    foreach (var item in getCountries)
+                    {
+                        var getDetails = (from x in context.Arrivals
+                                          where x.User.countryName == item
+                                          select x).FirstOrDefault();
+                        if (getDetails != null)
+                        {
+                            chart1.Series[0].Points.AddXY(item, getDetails.numberDelegate + getDetails.numberHead);
+                            chart1.Series[1].Points.AddXY(item, getDetails.numberCompetitors);
+                        }
+                    }
+                }
+            }
+            else if (cbTypeGuest.SelectedItem.ToString() == "Delegates")
+            {
+                using (var context = new Session3Entities())
+                {
+
+                    var getCountries = (from x in context.Users
+                                        select x.countryName).Distinct();
+                    foreach (var item in getCountries)
+                    {
+                        var getDetails = (from x in context.Arrivals
+                                          where x.User.countryName == item
+                                          select x).FirstOrDefault();
+                        if (getDetails != null)
+                        {
+                            chart1.Series[0].Points.AddXY(item, getDetails.numberDelegate + getDetails.numberHead);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                using (var context = new Session3Entities())
+                {
+
+                    var getCountries = (from x in context.Users
+                                        select x.countryName).Distinct();
+                    foreach (var item in getCountries)
+                    {
+                        var getDetails = (from x in context.Arrivals
+                                          where x.User.countryName == item
+                                          select x).FirstOrDefault();
+                        if (getDetails != null)
+                        {
+                            
+                            chart1.Series[1].Points.AddXY(item, getDetails.numberCompetitors);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void cbTypeGuest_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadGraph();
+        }
+    }
+}
