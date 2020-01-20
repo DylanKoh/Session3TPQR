855ec82d36976523803573d864f60ddedb32f5cc
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-01-21 00:08:16 +0800

    3.6 Documented

--- 3.6_HotelSelection.cs
+++ 3.6_HotelSelection.cs
@@ -23,12 +23,7 @@ namespace Session3
         {
         }
 
-        
-        private void button2_Click_1(object sender, EventArgs e)
-        {
-            (new Book_Hotel(_userID, 2)).Show();
-        }
-
+        //Redirects user back to Representative Main Menu page - 3.3
         private void backBtn_Click(object sender, EventArgs e)
         {
             this.Hide();
@@ -36,26 +31,37 @@ namespace Session3
             this.Close();
         }
 
+        //Pop-up of Pan Pacific Hotel Booking in Book Hotel page - 3.7
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            (new Book_Hotel(_userID, 2)).Show();
+        }
+
+        //Pop-up of Ritz-Carlton Hotel Booking in Book Hotel page - 3.7
         private void button1_Click_1(object sender, EventArgs e)
         {
             (new Book_Hotel(_userID, 1)).Show();
         }
 
+        //Pop-up of Charlton Hotel Booking in Book Hotel page - 3.7
         private void button3_Click(object sender, EventArgs e)
         {
             (new Book_Hotel(_userID, 3)).Show();
         }
 
+        //Pop-up of Intercontinental Singapore Hotel Booking in Book Hotel page - 3.7
         private void button4_Click(object sender, EventArgs e)
         {
             (new Book_Hotel(_userID, 4)).Show();
         }
 
+        //Pop-up of Hotel Grand Pacific Hotel Booking in Book Hotel page - 3.7
         private void button5_Click(object sender, EventArgs e)
         {
             (new Book_Hotel(_userID, 5)).Show();
         }
 
+        //Pop-up of Hotel Royal Queens Hotel Booking in Book Hotel page - 3.7
         private void button6_Click(object sender, EventArgs e)
         {
             (new Book_Hotel(_userID, 6)).Show();;
